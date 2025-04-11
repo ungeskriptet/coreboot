e7bc4dd487994514bde0dfcad9bea438c7d664c7
David Wronek <david@mainlining.org>
2025-04-11 22:36:08 +0200

    fixup! Add support for N650DU

--- src/mainboard/clevo/n650du/bootblock.c
+++ src/mainboard/clevo/n650du/bootblock.c
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <bootblock_common.h>
 #include <soc/gpio.h>
 
 /* Early pad configuration */
--- src/mainboard/clevo/n650du/romstage.c
+++ src/mainboard/clevo/n650du/romstage.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
 #include <fsp/api.h>
+#include <soc/romstage.h>
 #include <spd_bin.h>
 #include <string.h>
 
